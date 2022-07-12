ad3ac686351023191bf598c7e6f5f4ffb3385ab4
M La <mariela2610@gmail.com>
2022-07-12 15:32:51 +0200

    tests méthode createElement pour affichage  page Cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -14,11 +14,12 @@ console.log(cartItems);
 let basket = JSON.parse(localStorage.getItem("basket"));
 console.log(basket);
 
-// Création d'une boucle pour parcourir le tableau
+// Création d'une boucle pour parcourir l'objet
 for (let item of basket) {
   let itemId = item.id;
   let itemColor = item.color;
   let itemQuantity = item.quantity;
+  
 
   // Récuperation des infos API pour afficher chaque produit
   async function getResponseApi(itemId) {
@@ -29,41 +30,97 @@ for (let item of basket) {
 
     const response = await responseJSON.json();
     console.log(response, "objet Javascript");
+
+    //Création des balises
+    //Affichage des infos dans la page
+
+    //Création de la balise article et insertion dans balise section
+    let productArticle = document.createElement("article");
+    document.querySelector("#cart__items").appendChild(productArticle);
+    productArticle.className = "cart__item";
+    productArticle.setAttribute("data-id", itemId);
+    productArticle.setAttribute("data-color", itemColor);
+
+    //<div class"cart__item__img>
+    let divImg = document.createElement("div");
+    productArticle.appendChild(divImg);
+    divImg.className = "cart__item__img";
+
+    //<img>
+    let productImg = document.createElement("img");
+    divImg.appendChild(productImg);
+    productImg.src = response.imageUrl;
+    productImg.alt = response.altTxt;
+
+    //<div class="cart__item__content">
+    let divProductInfo = document.createElement("div");
+    productArticle.append(divProductInfo);
+    divProductInfo.className = "cart__item__content";
+
+    //<div class="cart__item__content__description">
+    let divProductDescription = document.createElement("div");
+    divProductInfo.appendChild(divProductDescription);
+    divProductDescription.className = "cart__item__content__description";
+
+    //Balise <h2>
+    let productName = document.createElement("h2");
+    divProductDescription.append(productName);
+    productName.innerText = response.name;
+
+    //Paragraphe couleur
+    let productColor = document.createElement("p");
+    divProductDescription.append(productColor);
+    productColor.innerHTML = "Couleur : " + itemColor;
+
+    //Paragraphe prix
+    let productPrice = document.createElement("p");
+    divProductDescription.append(productPrice);
+    productPrice.id = "unitPrice";
+    productPrice.innerHTML = "Prix : " + response.price + " €";
+
+    //<div class="cart__item__content__settings">
+    let divProductSettings = document.createElement("div");
+    divProductInfo.appendChild(divProductSettings);
+    divProductSettings.className = "cart__item__content__settings";
+
+    // <div class="cart__item__content__settings__quantity">
+    let divProductSettingsQuantity = document.createElement("div");
+    divProductSettings.append(divProductSettingsQuantity);
+    divProductSettingsQuantity.className =
+      "cart__item__content__settings__quantity";
+
+    //Paragraphe quantité
+    let productQuantity = document.createElement("p");
+    divProductSettingsQuantity.append(productQuantity);
+    productQuantity.innerHTML = "Quantité : ";
+
+    //Balise input choix quantité
+    let inputQuantity = document.createElement("input");
+    divProductSettingsQuantity.append(inputQuantity);
+    inputQuantity.type = "number";
+    inputQuantity.className = "itemQuantity";
+    inputQuantity.name = "itemQuantity";
+    inputQuantity.min = 1;
+    inputQuantity.max = 100;
+    inputQuantity.value = itemQuantity;
+
+    //Calcul du nombre total d'elements du panier
+    let totalItem = document.querySelector("#totalQuantity");
+    totalItem.innerHTML = item.quantity;
+
+    //Balise bouton delete
+    let divDeletebtn = document.createElement("div");
+    divProductSettings.append(divDeletebtn);
+    divDeletebtn.className = "cart__item__content__settings__delete";
+    let deleteBtn = document.createElement("p");
+    divDeletebtn.append(deleteBtn);
+    deleteBtn.className = "deleteItem";
+    deleteBtn.innerHTML = "Supprimer";
   }
-
-  //Stockage de la structure html
-  let contenuPanierHtml = "";
-
-  contenuPanierHtml += `
-      <article class="cart__item" data-id="${item.id}" data-color="${item.color}">
-                <div class="cart__item__img">
-                  <img src="${item.imageUrl}" alt="${item.altTxt}">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>${item.name}</h2>
-                    <p></p>
-                    <p>${item.price}</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.nombreItems}">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                  </div>
-                </div>
-              </article> `;
-
-  //Affichage des produits du panier
-
-  document.querySelector("#cart__items").innerHTML += contenuPanierHtml;
-  console.log(contenuPanierHtml);
-
-  getResponseApi(itemId).then();
-
+ getResponseApi(itemId).then();
 }
+
+
+
 
 
